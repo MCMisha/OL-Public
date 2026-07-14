471832cf5479dea00e5166f1f348aba92954f612
Mikhail Yakushevich <misha.yakushevich4@gmail.com>
2026-07-14 13:26:45 +0200

    Add files via upload

---
+++ Interfaces/Converters/NewsConverter.cs
@@ -0,0 +1,25 @@
+﻿using WebApplicationOperaLublin.Interfaces.Converters;
+using WebApplicationOperaLublin.Models;
+using WebApplicationOperaLublin.Models.DTO;
+using WebApplicationOperaLublin.Models.Enums;
+
+namespace WebApplicationOperaLublin.Converters;
+
+public class NewsConverter : INewsConverter
+{
+    public News ConvertFromViewModelToModel(NewsCreateUpdateDto createUpdateDto)
+    {
+        return new News
+        {
+            Id = createUpdateDto.Id,
+            Title = createUpdateDto.Title,
+            Subtitle = createUpdateDto.Subtitle,
+            MainImage = string.IsNullOrEmpty(createUpdateDto.MainImage)
+                ? null!
+                : Convert.FromBase64String(createUpdateDto.MainImage)!,
+            Content = createUpdateDto.Content,
+            CreationDate = createUpdateDto.CreationDate,
+            Category = (NewsCategory)createUpdateDto.Category
+        };
+    }
+}
---
+++ Interfaces/Converters/PerformanceEventConverter.cs
@@ -0,0 +1,21 @@
+﻿using WebApplicationOperaLublin.Interfaces.Converters;
+using WebApplicationOperaLublin.Models;
+using WebApplicationOperaLublin.Models.DTO;
+
+namespace WebApplicationOperaLublin.Converters;
+
+public class PerformanceEventConverter : IPerformanceEventConverter
+{
+    public PerformanceEventDto ConvertFromPerformanceEventToPerformanceEventDto(PerformanceEvent model)
+    {
+        return new PerformanceEventDto
+        {
+            Id = model.Id,
+            Title = model.Performance.Title,
+            StartAt = model.StartAt,
+            Genre = model.Performance.GenreNavigation.Name,
+            PerformanceId = model.PerformanceId,
+            Place = model.Performance.PlaceNavigation.Name,
+        };
+    }
+}
