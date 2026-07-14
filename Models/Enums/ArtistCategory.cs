153b2354c1854ab74caf78fb4af9ff6cb9ed5f8f
Mikhail Yakushevich <misha.yakushevich4@gmail.com>
2026-07-14 13:42:51 +0200

    Add ArtistCategory enum for artist classifications

---
+++ Models/Enums/ArtistCategory.cs
@@ -0,0 +1,33 @@
+namespace WebApplicationOperaLublin.Models.Enums;
+
+public enum ArtistCategory
+{
+    Solista = 1,
+    Dyrygent = 2,
+    ZespolOrkiestry = 3,
+
+    SkrzypceI = 10,
+    SkrzypceII = 11,
+    Altowki = 12,
+    Wiolonczele = 13,
+    Kontrabasy = 14,
+
+    Flety = 20,
+    Oboje = 21,
+    Klarnety = 22,
+    Fagoty = 23,
+
+    Trabki = 30,
+    Waltornie = 31,
+    Puzony = 32,
+
+    Perkusja = 40,
+    Gitara = 41,
+    Akordeon = 42,
+    Harfa = 43,
+
+    ZespolChoru = 50,
+    ZespolBaletu = 51,
+    
+    Wspolpraca = 60
+}
